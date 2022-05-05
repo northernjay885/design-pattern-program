545ec06b20d68836b92b97c9a2a3f402b5ab9f7a
NorthernJay <lrj193927@gmail.com>
2022-05-05 09:21:08 -0400

    create state example

---
+++ behavior_design_pattern/state/Event.java
@@ -0,0 +1,6 @@
+package behavior_design_pattern.state;
+
+public class Event {
+
+    public boolean doubleClick;
+}
---
+++ behavior_design_pattern/state/ReadyState.java
@@ -0,0 +1,38 @@
+package behavior_design_pattern.state;
+
+public class ReadyState extends State {
+
+    Event event;
+    public ReadyState(AudioPlayer player) {
+        super(player);
+    }
+
+    @Override
+    public void clickLock() {
+        player.changeState(new LockState(player));
+    }
+
+    @Override
+    public void clickPlay() {
+        player.startPlayback();
+        player.changeState(new PlayingState(player));
+    }
+
+    @Override
+    public void clickNext() {
+        if (event.doubleClick) {
+            player.nextSong();
+        } else {
+            player.fastForward(5);
+        }
+    }
+
+    @Override
+    public void clickPrevious() {
+        if (event.doubleClick) {
+            player.previousSong();
+        } else {
+            player.rewind(5);
+        }
+    }
+}
