2958ef44e07ddd8b94010c530a4eb704e77f14a9
NorthernJay <lrj193927@gmail.com>
2022-05-06 09:01:17 -0400

    create template method example

---
+++ behavior_design_pattern/template_method/MonstersAI.java
@@ -0,0 +1,23 @@
+package behavior_design_pattern.template_method;
+
+public class MonstersAI extends GameAI {
+    @Override
+    void buildStructure() {
+
+    }
+
+    @Override
+    void buildUnits() {
+
+    }
+
+    @Override
+    void sendScouts(int position) {
+
+    }
+
+    @Override
+    void sendWarriors(int position) {
+
+    }
+}
